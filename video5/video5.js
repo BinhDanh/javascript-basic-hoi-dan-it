566044c15573aed19fc10e75501a2bf7041680d7
BinhDanh <danhbinh943@gmail.com>
2025-03-08 14:57:38 +0700

    video 5-6

---
+++ video5/video5.js
@@ -0,0 +1,5 @@
+console.log('Hello world from HTML')
+
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7, 13);
+console.log(part)
---
+++ video6/video6.js
@@ -0,0 +1,16 @@
+console.log('Hello world from HTML'.toUpperCase())
+let a = 5; //int, float, long, double
+let b = 6;
+let c = 6.9;
+let d = '5'
+// let e = Number(d)
+let e = +d; //convert kiểu string --> kiểu number dùng dấu +
+
+// console.log(a + b)
+// console.log(a + c)
+// console.log(a * d)
+// console.log(a / d)
+
+console.log('e value: ', e, 'type: ', typeof e)
+// So sánh number và string
+console.log(a === e, 'a:', typeof a, 'd:', typeof d)
