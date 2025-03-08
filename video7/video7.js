386ac8dbb1e186afb6fb3ecb95100760052c18f8
BinhDanh <danhbinh943@gmail.com>
2025-03-08 15:50:32 +0700

    video 7

---
+++ video7/video7.js
@@ -0,0 +1,15 @@
+console.log('Hello world from HTML')
+//Templates string
+
+const a = 5;
+const b = 10;
+console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
+console.log("Fifteen is " + a + b + " and\nnot " + (2 * a + b) + ".");
+
+// url: 'hoidanit/5'
+console.log(`Hoi dan it check string: a = ${a} b = ${b} a + b = ${a+b}`)
+console.log(`/hoidanit/${a}`)
+
+const name = "Alice";
+const age = 25;
+console.log(`Hello, my name is ${name} and I am ${age} years old.`);
