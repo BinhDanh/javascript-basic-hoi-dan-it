d8e377258c57a508641f40c3c324da021786d472
BinhDanh <danhbinh943@gmail.com>
2025-03-08 18:54:56 +0700

    video 9

---
+++ video9/video9.js
@@ -0,0 +1,19 @@
+console.log('Hello world from HTML');
+let a = {
+    name: 'Binh',
+    age: 22
+}; //key: value
+
+console.log('check data object: ', a)
+console.log('my name\'s: ', a.name)
+
+//array = [item1, item2, item3]
+let b = ['MU', 'Chelsea', 'Liver'];
+console.log('typeof b:', typeof b, b)
+console.log('first item: ', b[0], 'second item: ', b[1])
+
+let family = [
+    { name: 'Eric', age: '25'},
+    { name: 'Alice', age: '23'}
+]
+console.log('my family: ', family)
