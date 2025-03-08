c18e93dff05381569433cb50d86634b7113506f1
BinhDanh <danhbinh943@gmail.com>
2025-03-08 18:10:34 +0700

    video 8

---
+++ video8/video8.js
@@ -0,0 +1,28 @@
+console.log('Hello world from HTML')
+// let a = {}
+// let b = 'Binh'
+// console.log('type of a: ', typeof a, 'type of b: ', typeof b)
+
+//key: value
+let obj = {
+    name: 'Binh',
+    address: 'Kien Giang',
+    email: '',
+    gender: '',
+    // a: function() {
+    //     console.log('Hello world inside function')
+    //     return ''
+    // }
+};
+
+let b = 'address';
+
+//obj[b] = 'Hary'
+obj.name = 'HCM'
+
+console.log('What is your name ?', `My name 's:`, obj.name)
+console.log('Where are you from ?', `I'am from :`, obj.address)
+// console.log('call function:', obj.a())
+
+
+
